96637b144994fc75de515d029d903289502f9361
Rishi <Rishi@Rishis-MacBook-Pro.local>
2015-10-26 00:54:08 +0530

    Added the initial game of life; seems to be working

---
+++ GameOfLife.hpp
@@ -0,0 +1,18 @@
+//
+//  GameOfLife.hpp
+//  AI
+//
+//  Created by Rishi on 20/10/15.
+//  Copyright © 2015 Rishi. All rights reserved.
+//
+
+#ifndef GameOfLife_hpp
+#define GameOfLife_hpp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <string>
+
+#endif /* GameOfLife_hpp */
