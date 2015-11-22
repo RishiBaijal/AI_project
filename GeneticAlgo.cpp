ee46a2aa984178c4cf552dd7ae5d2fbd5bcae172
Rishi <Rishi@Rishis-MacBook-Pro.local>
2015-11-23 03:15:04 +0530

    Added optimized code

--- GeneticAlgo.cpp
+++ GeneticAlgo.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 #define MAX_SIZE 1000
-#define TIME_END 10000
+#define TIME_END 1000
 
 #include <iostream>
 #include <vector>
@@ -12,7 +12,7 @@ using namespace std;
 #include <time.h>
 #include <math.h>
 
-#define POPULATION_SIZE 2000
+#define POPULATION_SIZE 100
 #define MAX_ITER 100
 #define ELITE_RATE 0.10f
 #define MUTATION_RATE 0.25f
@@ -44,7 +44,7 @@ colony prot;
 
 
 typedef vector<ga_struct> ga_vector;
-typedef vector<colony> col_vector;
+typedef vector<colony*> col_vector;
 
 
 void copy(vector<bool> source[MAX_SIZE], vector<bool> dest[MAX_SIZE])
@@ -108,9 +108,14 @@ long long int getalivecells(vector<bool> array[MAX_SIZE], int genCount)
     }
     return alive_now;
 }
-bool life_state(vector<bool> arr[MAX_SIZE])
+
+
+
+pair<bool,long long int> life_state(vector<bool> arr[MAX_SIZE])
 {
-    alive=0;
+    pair<char, double> twoValues;
+    
+    long long int aliven=0;
     int flag=false;
     int i, j, count;
     bool prev_row[COL_NUMBER];
@@ -152,20 +157,20 @@ bool life_state(vector<bool> arr[MAX_SIZE])
                 flag=true;
             
             if(arr[i][j])
-                alive++;
+                aliven++;
         }
         prev=false;
         prev_row[COL_NUMBER-2]=this_row[COL_NUMBER-2];
         this_row[COL_NUMBER-2]=arr[i+1][COL_NUMBER-2];
     }
-    return flag;
+    twoValues.first=flag;
+    twoValues.second=aliven;
+    return twoValues;
 }
 
 long long int debugscore=0;
 long long int run_simulation(vector<bool> gen0[MAX_SIZE],long long int endoftime=100)
 {
-    int i, j;
-    char run_again;
     string sanity_check;
     bool change = false;
     
@@ -177,9 +182,9 @@ long long int run_simulation(vector<bool> gen0[MAX_SIZE],long long int endoftime
     {
         // system("sleep 0.1");
         // system("clear");
-        
-        change  =life_state(gen0);
-        finalscore=getalivecells(gen0, genCount);
+        pair<bool,long long int> anspair=life_state(gen0);
+        change  =anspair.first;
+        finalscore=anspair.second;
         overallscore+=finalscore;
         
         /*
@@ -193,12 +198,12 @@ long long int run_simulation(vector<bool> gen0[MAX_SIZE],long long int endoftime
          */
         genCount++;
     }
-    while (change && genCount<endoftime);//upper limit is endofline. change it to user inpt.
-    if(overallscore>debugscore)
-        debugscore=overallscore;
+    while (genCount<endoftime);//upper limit is endofline. change it to user inpt.
+    //   if(overallscore>debugscore)
+    //      debugscore=overallscore;
     //printf("Debug score:%lld\n",debugscore);
     
-    return overallscore;
+    return overallscore ;//finalscore
 }
 
 long long int initialize(vector<bool> arr[MAX_SIZE])
@@ -243,7 +248,7 @@ void userinteract()
 {
     //    long long int n,m;
     
-    char run_again;
+//    char run_again;
     //struct organism* prototype=(organism*)malloc(sizeof(organism));
     //organism prot;
     
@@ -272,44 +277,27 @@ void userinteract()
 
 
 
-// void init_population(ga_vector &population, ga_vector &buffer)
-// {
-//     int tsize = GA_TARGET.size();
-//     int i, j;
-//     for (i = 0; i < POPULATION_SIZE; i++)
-//     {
-//         ga_struct citizen;
-//         citizen.fitness = 0;
-//         citizen.str.erase();
-//         for (j = 0; j < tsize; j++)
-//         {
-//             citizen.str+=(rand() % 90) + 32;
-//         }
-//         population.push_back(citizen);
-
-//     }
-//     buffer.resize(POPULATION_SIZE);
-// }
-
 
 void init_population1(col_vector &population, col_vector &buffer)
 {
-    int tsize = GA_TARGET.size();
-    int i, j;
+    //    int tsize = GA_TARGET.size();
+    int i;
     for (i = 0; i < POPULATION_SIZE; i++)
     {
         
-        colony col;
-        col.fitness = 0;
+        colony* col=(colony*)malloc(sizeof(colony));
+        
+        col->fitness = 0;
         //citizen.str.erase();
-        long long int gotalive=initialize(col.gen);
-        col.init_alive=gotalive;
+        long long int gotalive=initialize(col->gen);
+        col->init_alive=gotalive;
         
         //        printf("do: %lld\n",col.init_alive);
         population.push_back(col);
+        buffer.push_back(col);
         
     }
-    buffer.resize(POPULATION_SIZE);
+    // buffer.resize(POPULATION_SIZE);
 }
 
 
@@ -319,147 +307,140 @@ void calculate_fitness1(col_vector &population)
     //   string target = GA_TARGET;
     //    int tsize = target.size();
     unsigned int fitness;
-    int i, j;
+    colony* temp=(colony*)malloc(sizeof(colony));
+    int i;
     for (i = 0; i < POPULATION_SIZE; i++)
     {
         fitness = 0;
-        colony tempcol=population[i];
-        fitness=run_simulation(tempcol.gen,upperbound);
+        
+        *temp = *(population[i]);
+        
+        fitness=run_simulation(temp->gen,upperbound);
         //  cout<<"Fitness= "<<fitness<<endl;
-        population[i].fitness = fitness;
+        population[i]->fitness = fitness;
     }
 }
 
-// bool fitness_sort(ga_struct x, ga_struct y)
-// {
-//     return (x.fitness < y.fitness);
-// }
-//
-//bool fitness_sort1(colony x, colony y)
-//{
-//    return (x.fitness > y.fitness);
-//}
 
 struct Comparator
 {
-    bool operator()(colony x, colony y)
+    bool operator()(colony *x, colony *y)
     {
-        return (x.fitness > y.fitness);
+        return (x->fitness) > (y->fitness)  ;
     }
 };
 
 
-inline void sort_by_fitness1(col_vector &population)
+bool fitness_sort1(colony *x, colony *y)
 {
-    sort(population.begin(), population.end(), Comparator());
+    return (x->fitness) > (y->fitness);
 }
 
-// void elitism(ga_vector &population, ga_vector &buffer, int esize)
-// {
-//     int i;
-//     for (i = 0; i < esize; i++)
-//     {
-//         buffer[i].str = population[i].str;
-//         buffer[i].fitness = population[i].fitness;
-//     }
-// }
+
+inline void sort_by_fitness1(col_vector &population)
+{
+    //  printf("begin sorting %lu\n",(population).size());
+    sort(population.begin(), population.end(), Comparator());
+    //  printf("end sorting\n");
+    
+}
+
 
 
 void elitism1(col_vector &population, col_vector &buffer, int esize)
 {
     int i;
+    colony* temp=(colony*)malloc(sizeof(colony));
+    
     for (i = 0; i < esize; i++)
     {
         //   buffer[i].gen = population[i].gen;
         //    buffer[i].fitness = population[i].fitness;
-        buffer[i]=population[i];
+        *temp=*population[i];
+        buffer[i]=temp;
         
     }
 }
-// void mutate(ga_struct &member)
-// {
-//     int tsize = GA_TARGET.size();
-//     int ipos = rand() % tsize;
-//     int delta = (rand() % 90) + 32;
 
-//     member.str[ipos] = ((member.str[ipos] + delta) % 122);
-// }
 
-void mutate1(colony &member)
+
+void mutate1(colony *member)
 {
     
     int ipos = rand() % ROW_NUMBER;
     int jpos = rand() % COL_NUMBER;
     //    int delta = (rand() % 90) + 32;
     
-    member.gen[ipos][jpos] = !member.gen[ipos][jpos];
+    member->gen[ipos][jpos] = !(member->gen[ipos][jpos]);
     
 }
 
-void mutate2(colony &member)
+void mutate2(colony *member)
 {
     int ipos = rand() % ROW_NUMBER;
     int jpos = rand() % COL_NUMBER;
     //    int delta = (rand() % 90) + 32;
     for(jpos=1;jpos<COL_NUMBER-1;jpos++)
-        member.gen[ipos][jpos] = !member.gen[ipos][jpos];
+        member->gen[ipos][jpos] = !(member->gen[ipos][jpos]);
 }
 
-// void mate(ga_vector &population, ga_vector &buffer)// this is where the fucking happens
-// {
-//     int esize = POPULATION_SIZE * ELITE_RATE;
-//     int tsize = GA_TARGET.size(), spos, i1, i2, i;
-//     elitism(population, buffer, esize);
-//     for (i = esize; i < POPULATION_SIZE; i++)
-//     {
-//         i1 = rand() % (POPULATION_SIZE / 2);
-//         i2 = rand() % (POPULATION_SIZE / 2);
-//         spos = rand() % tsize;
-
-//         buffer[i].str = population[i1].str.substr(0, spos) + population[i2].str.substr(spos, esize - spos);
-
-//         if (rand() < MUTATION)
-//         {
-//             mutate(buffer[i]);
-//         }
-//     }
-// }
 
 void mate1(col_vector &population, col_vector &buffer)// this is where the fucking happens
 {
     int esize = POPULATION_SIZE * ELITE_RATE;
-    int tsize = ROW_NUMBER, spos, i1, i2, i;
-    elitism1(population, buffer, esize);
+    long long int tsize = ROW_NUMBER, spos, i1, i2, i;
+    
+    
+    //elitism1(population, buffer, esize);
+    //int i;
+    for (i = 0; i < esize; i++)
+    {
+        //    buffer[i]=(colony*)malloc(sizeof(colony));
+        *buffer[i]=*population[i];;
+        
+    }
+    
+    
     for (i = esize; i < POPULATION_SIZE; i++)
     {
+        //   buffer[i]=(colony*)malloc(sizeof(colony));
+        
         i1 = rand() % (POPULATION_SIZE / 2);
         i2 = rand() % (POPULATION_SIZE / 2);
         spos = rand() % tsize;
         int j;
+        
         for(j=0;j<spos;j++)
         {
-            buffer[i].gen[j]=population[i1].gen[j];
+            (*buffer[i]).gen[j]=(*population[i1]).gen[j];
         }
+        
         for(;j<ROW_NUMBER;j++)
         {
-            buffer[i].gen[j]=population[i2].gen[j];
+            (*buffer[i]).gen[j]=(*population[i2]).gen[j];
         }
-        //        buffer[i].str = population[i1].str.substr(0, spos) + population[i2].str.substr(spos, esize - spos);
         
         if (rand() < MUTATION)
         {
-            mutate1(buffer[i]);
+            //   mutate1(*buffer[i]);
+            int ipos = rand() % ROW_NUMBER;
+            int jpos = rand() % COL_NUMBER;
+            
+            //mutate method 1
+            (*buffer[i]).gen[ipos][jpos] = !((*buffer[i]).gen[ipos][jpos]);
+            
+            //mutate method 1
+            //    for(jpos=1;jpos<COL_NUMBER-1;jpos++)
+            //         (*buffer[i]).gen[ipos][jpos] = !((*buffer[i]).gen[ipos][jpos]);
+            
         }
+        
     }
+    
+    
 }
 
 
-// inline void swap(ga_vector *&population, ga_vector *&buffer)
-// {
-//     ga_vector *temp = population;
-//     population = buffer;
-//     buffer = temp;
-// }
 
 inline void swap1(col_vector *&population, col_vector *&buffer)
 {
@@ -468,16 +449,13 @@ inline void swap1(col_vector *&population, col_vector *&buffer)
     buffer = temp;
 }
 
-// inline void print_best(ga_vector ga)
-// {
-//     cout<<"String: "<< ga[0].str<<"\n";
-//     cout<<"Fitness value: "<< ga[0].fitness<<"\n";
-// }
-inline void print_best1(col_vector ga)
+
+
+inline void print_best1(col_vector &ga)
 {
     //cout<<"String: "<< ga[0].str<<"\n";
     //printArr(ga[0].gen,0);
-    cout<<"Fitness value: "<< ga[0].fitness<<"\n";
+    cout<<"Fitness value: "<< (*ga[0]).fitness<<"\n";
 }
 
 
@@ -485,6 +463,96 @@ ga_vector init_pop, init_buffer, *population1, *buffer1;
 
 col_vector init_cols, init_buffers, *colonies1, *buffers1;
 
+void printsidebyside(vector<bool> gen1[MAX_SIZE],vector<bool> gen2[MAX_SIZE], long long int genCount)
+{
+    long long int alive_now1=0;
+    long long int alive_now2=0;
+    int i, j;
+    
+    
+    printf("\n");
+    for (i = 1; i < ROW_NUMBER - 1; i++)
+    {   printf("  ");
+        for (j = 1; j < COL_NUMBER - 1; j++)
+        {
+            if (gen1[i][j]){
+                cout<<"#"<<" ";
+                alive_now1++;
+            }
+            else{
+                cout<<"."<<" ";
+            }
+        }
+        cout<<"\t";
+        printf("  ");
+        for (j = 1; j < COL_NUMBER - 1; j++)
+        {
+            if (gen2[i][j]){
+                cout<<"#"<<" ";
+                alive_now2++;
+            }
+            else{
+                cout<<"."<<" ";
+            }
+        }
+        printf("\n");
+    }
+    printf("\n--------------------------------\n GENERATION %lld : \nLife Count: %lld - %lld\n--------------------------------\n ",genCount,alive_now1, alive_now2);
+    
+}
+
+void runsidebyside(colony* a, colony*b,long long int endoftime)
+{
+    string sanity_check;
+    bool change1 = false;
+    bool change2 = false;
+    long long int overallscore1=0;
+    long long int finalscore1=0;
+    // system("clear");
+    long long int overallscore2=0;
+    long long int finalscore2=0;
+    int genCount = 0;
+    pair<bool,long long int> anspair;
+    do
+    {
+        if(change1 || change2){
+            system("sleep 0.1");
+        }
+        system("clear");
+        
+        printsidebyside(a->gen,b->gen,genCount);
+        anspair=life_state(a->gen);
+        change1  =anspair.first;
+        finalscore1=anspair.second;
+        overallscore1+=finalscore1;
+        
+        anspair=life_state(b->gen);
+        change2  =anspair.first;
+        finalscore2=anspair.second;
+        overallscore2+=finalscore2;
+        printf("\nOverall Count: %lld - %lld\n--------------------------------\n ",overallscore1, overallscore2);
+        
+        printf("\n");
+        
+        /*
+         if(genCount>1 && genCount%100==0)
+         {
+         printf("\nContinue Simulation?:");
+         cin>>run_again;
+         if(run_again!='y')
+         break;
+         }
+         */
+        genCount++;
+    }
+    while (genCount<endoftime);
+    
+//    long long int alive_now1=0;
+//    long long int alive_now2=0;
+    
+    printf("\n");
+    
+}
 int main()
 {
     
@@ -496,41 +564,47 @@ int main()
     //  init_population(init_pop, init_buffer);
     colonies1 = &init_cols;
     buffers1 = &init_buffers;
-    int i;
+    int i=0;
+//    long long int firstfitness=0;
+    colony* first_best_colony=(colony*)malloc(sizeof(colony));
     for (i = 0; i < MAX_ITER; i++)
     {
-        //    printf("ITERATION %d\n\n\n\n",i);
-        //    calculate_fitness(*population1);
-        printf("Calculating fitness of colonies\n");
+        //printf("ITERATION %d\n\n\n\n",i);
+        
+        //printf("Calculating fitness of colonies\n");
         calculate_fitness1(*colonies1);
-        //sort_by_fitness(*population1);
-        printf("Sorting fitness of colonies\n");
+        //printf("Sorting fitness of colonies\n");
         sort_by_fitness1(*colonies1);
-        int j;
+        //     int j;
         /*
          for(j=0;j<POPULATION_SIZE;j++)
          {
          printf("Fitness= %u\n",init_cols[j].fitness);
          }
          */
-        //print_best(*population1);
-        printf("Printing the best colony fitness\n");
+        //  printf("Printing the best colony fitness\n");
         
         print_best1(*colonies1);
+        if(i==0)
+        {
+            *first_best_colony= *(*colonies1)[0];
+        }
+        
         //if ((*population1)[0].fitness == 0)
         //{
         //    break;
         //}
-        //mate(*population1, *buffer1);
-        printf("Mating colonies\n");
+        // printf("Mating colonies\n");
         
         mate1(*colonies1,*buffers1);
-        printf("Swapping colonies\n");
+        // printf("Swapping colonies\n");
         
-        //swap(population1, buffer1);
         swap1(colonies1,buffers1);
-        printf("\nDone with iteration:%d\n",i);
+        
+        //   printf("\nDone with iteration:%d\n",i);
     }
+    
+    runsidebyside(first_best_colony,(*colonies1)[0],upperbound);
     return 0;
     
 }
